fb76fa709d0afb479c678777fe218f194f942615
sacha11 <s.j.g_97@hotmail.com>
2025-01-06 18:40:33 -0300

    tranformation almost done

--- scripts/RedditPipeline/Scripts/TransformData.py
+++ scripts/RedditPipeline/Scripts/TransformData.py
@@ -15,3 +15,12 @@ os.chdir(os.path.join(FilePath, '../'))
 ProjectPath = os.getcwd()
 print(ProjectPath)
 FilesFolderPath = os.path.join(ProjectPath, r'Files')
+
+def transform_data(post_df: pd.DataFrame):
+    post_df['created_utc'] = pd.to_datetime(post_df['created_utc'], unit='s')
+    post_df['over_18'] = np.where((post_df['over_18'] == True), True, False)
+    post_df['author'] = post_df['author'].astype(str)
+    return post_df
+
+def load_data_to_csv(data: pd.DataFrame, path: str):
+    data.to_csv(path, index=False)
