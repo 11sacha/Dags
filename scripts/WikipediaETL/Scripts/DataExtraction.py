fbfcc0aafc8ebb665821825f6a2217b559e6ef5b
sacha11 <s.j.g_97@hotmail.com>
2025-01-16 15:31:39 -0300

    data extraction done

--- scripts/WikipediaETL/Scripts/DataExtraction.py
+++ scripts/WikipediaETL/Scripts/DataExtraction.py
@@ -30,3 +30,45 @@ def get_wikipedia_data(html):
 
     return table_rows
 
+def clean_text(text):
+    text = str(text).strip()
+    text = text.replace('&nbsp', '')
+    if text.find(' ♦️'):
+        text = text.split(' ♦️')[0]
+    if text.find('[') != -1:
+        text = text.split('[')[0]
+    if text.find(' (formerly)') != -1:
+        text = text.split(' (formerly)')[0]
+
+    return text.replace('\n', '')
+
+def run_process(**kwargs):
+
+    try:
+        url = kwargs['url']
+        html = get_wikipedia_page(url)
+        rows = get_wikipedia_data(html)
+
+        data = []
+
+        for i in range(1, len(rows)):
+            tds = rows[i].find_all('td')
+            values = {
+                'rank': i,
+                'stadium': clean_text(tds[0].text),
+                'capacity': clean_text(tds[1].text).replace(',', '').replace('.', ''),
+                'region': clean_text(tds[2].text),
+                'country': clean_text(tds[3].text),
+                'city': clean_text(tds[4].text),
+                'images': 'https://' + tds[5].find('img').get('src').split("//")[1] if tds[5].find('img') else "NO_IMAGE",
+                'home_team': clean_text(tds[6].text),
+            }
+            data.append(values)
+
+        json_rows = json.dumps(data)
+        kwargs['ti'].xcom_push(key='rows', value=json_rows)
+
+        return 'Data extracted'
+
+    except Exception as e:
+        print(f"There's been an error: {e}")
